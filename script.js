34cda8a63bd9b9b89c2792c9a008755f94d3cd78
Godwinbaa <godwinbaa01@gmail.com>
2025-07-19 20:06:53 +0530

    script

---
+++ script.js
@@ -0,0 +1,36 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherbox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+
+search.addEventListener('click', () => {
+
+    const APIKey = '98740f4ebc063bc0f8ba70090e5a091';
+    const city = document.querySelector('.search-box input').value;
+
+    if (city == '')
+        return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(json => {
+
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
+
+    switch (json.weather[0].main) {
+        case 'Clear':
+        image.src = 'images/clear.png';
+        break;
+
+    default:
+        break;
+    }
+
+
+        });
+
+});
